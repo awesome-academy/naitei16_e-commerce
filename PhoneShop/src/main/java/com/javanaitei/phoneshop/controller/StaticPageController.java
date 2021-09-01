7eb95735ddec2d5462c32daef82b1aa2551ef071
truong hung <truonghungvatvo@gmail.com>
2021-09-01 11:05:07 +0700

    chia layout

---
+++ PhoneShop/src/main/java/com/javanaitei/phoneshop/controller/StaticPageController.java
@@ -0,0 +1,25 @@
+package com.javanaitei.phoneshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class StaticPageController {
+    @GetMapping ("/about")
+    public String about() {
+        return "pages/about";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "pages/contact";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "pages/contact";
+    }
+}
