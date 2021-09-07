522812a9c7cea15b902619cb39a6609f4b3066d3
truong hung <truonghungvatvo@gmail.com>
2021-09-07 09:16:38 +0700

    pull master
    interface

---
+++ PhoneShop/src/main/java/com/javanaitei/phoneshop/controller/admin/AccountController.java
@@ -0,0 +1,17 @@
+package com.javanaitei.phoneshop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AccountController {
+    @GetMapping("/accounts")
+    public String users(Model model) {
+        String[] accounts = { "Honda", "BMW", "Ford", "Mazda", "Honda1", "BMW1", "Ford1", "Mazda1", "Honda2", "BMW2", "Ford2", "Mazda2" };
+        model.addAttribute("accounts", accounts);
+        return "admin/accounts/accounts";
+    }
+}
